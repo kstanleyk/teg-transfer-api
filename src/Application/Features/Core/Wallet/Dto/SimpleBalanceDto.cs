4f6fe41110c152d5e95097495d9417c94e5ce26d
Stanley Kiven <kstanleyk@live.com>
2025-10-11 17:24:44 +0100

    update wallet balance

---
+++ src/Application/Features/Core/Wallet/Dto/SimpleBalanceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TegWallet.Application.Features.Core.Wallet.Dto;
+
+public class SimpleBalanceDto
+{
+    public Guid WalletId { get; set; }
+    public decimal AvailableBalance { get; set; }
+    public string CurrencyCode { get; set; } = null!;
+}
--- src/Infrastructure/Migrations/20251011151635_Reservations.Designer.cs
+++ src/Infrastructure/Migrations/20251011151635_Reservations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TegWallet.Infrastructure.Persistence.Context;
@@ -11,9 +12,11 @@ using TegWallet.Infrastructure.Persistence.Context;
 namespace TegWallet.Infrastructure.Migrations
 {
     [DbContext(typeof(TransferContext))]
-    partial class TransferContextModelSnapshot : ModelSnapshot
+    [Migration("20251011151635_Reservations")]
+    partial class Reservations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
